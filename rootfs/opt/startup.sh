8274852d6fdd7ca2c9776d84e4542b019d595853
Bodo Schulz <support@xanhaem.de>
2017-02-12 14:04:28 +0100

    strip down and rebuild

--- rootfs/opt/startup.sh
+++ rootfs/opt/startup.sh
@@ -76,17 +76,6 @@ EOF
 }
 
 
-startSupervisor() {
-
-  echo -e "\n Starting Supervisor.\n\n"
-
-  if [ -f /etc/supervisord.conf ]
-  then
-    /usr/bin/supervisord -c /etc/supervisord.conf >> /dev/null
-  fi
-}
-
-
 run() {
 
   if [ ! -z ${MYSQL_BIN} ]
