aed9f842cde23d534bafafdf1fe42cc4fcb9b601
Bodo Schulz <bodo.schulz@coremedia.com>
2018-06-01 11:26:38 +0200

    spielereien mit travis

--- .travis/export_environment.sh
+++ .travis/export_environment.sh
@@ -4,4 +4,6 @@ wget http://dl-cdn.alpinelinux.org/alpine/latest-stable/main/x86_64/APKINDEX.tar
 tar -xzf APKINDEX.tar.gz
 version=$(grep -A1 "P:mariadb-common" APKINDEX | tail -n1 | cut -d ':' -f2 | cut -d '-' -f1)
 
-echo "${version}" > ${TRAVIS_BUILD_DIR}/.deployment-env
+export MARIADB_VERSION=${version}
+
+# echo "${version}" > ${TRAVIS_BUILD_DIR}/.deployment-env
