e7232107132c5651d1682f46d5cf5384e0a16b2b
Bodo Schulz <bodo.schulz@coremedia.com>
2018-06-01 10:39:33 +0200

    export MARIADB_VERSION in helper script

---
+++ .travis/export_environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget http://dl-cdn.alpinelinux.org/alpine/latest-stable/main/x86_64/APKINDEX.tar.gz
+tar -xzf APKINDEX.tar.gz
+version=$(grep -A1 "P:mariadb-common" APKINDEX | tail -n1 | cut -d ':' -f2 | cut -d '-' -f1)
+
+export MARIADB_VERSION=${version}
