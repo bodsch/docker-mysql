d2d1a1b62624e5e46938eb43066d194342feaaec
Bodo Schulz <bodo.schulz@coremedia.com>
2018-06-04 13:35:22 +0200

    setter and getter for consul k/v store

--- rootfs/init/consul.sh
+++ rootfs/init/consul.sh
@@ -1,54 +1,100 @@
 #!/bin/sh
 
-set -x
-
-if [[ -z "${CONSUL}" ]]
+if [[ -z "${CONSUL}" ]] || [[ -z "${CONFIG_BACKEND}"  ]]
 then
   return
 fi
 
-register()  {
+log_info "use '${CONFIG_BACKEND}' as configuration backend"
 
-#  data=$(curl \
-#    --request PUT \
-#    ${CONSUL}:8500/v1/agent/service/register \
-#    --data "{
-#      \"ID\": \"${HOSTNAME}\",
-#      \"Name\":\"${HOSTNAME}\",
-#      \"Port\": 3306,
-#      \"tags\": [\"database\"]
-#    }")
-#
-#  echo "${data}"
+wait_for_consul() {
+
+  RETRY=50
+  local http_response_code=0
+
+  until [[ 200 -ne $http_response_code ]] && [[ ${RETRY} -le 0 ]]
+  do
+    http_response_code=$(curl \
+      -w %{response_code} \
+      --silent \
+      --output /dev/null \
+      ${CONSUL}:8500/v1/health/service/consul)
+
+    [[ 200 -eq $http_response_code ]] && break
+
+    sleep 5s
+
+    RETRY=$(expr ${RETRY} - 1)
+  done
+
+  if [[ ${RETRY} -le 0 ]]
+  then
+    log_error "could not connect to the consul master instance '${CONSUL}'"
+    exit 1
+  fi
+}
+
+register_node()  {
+
+  local address=$(hostname -i)
+
+  data=$(curl \
+    --silent \
+    --request PUT \
+    ${CONSUL}:8500/v1/agent/service/register \
+    --data '{
+      "ID": "'${HOSTNAME}'",
+      "Name": "'${HOSTNAME}'",
+      "Port": 3306,
+      "Address": "'${address}'",
+      "tags": ["database"]
+    }')
+
+  echo "${data}"
+}
+
+set_consul_var() {
+
+  local consul_key=${1}
+  local consul_var=${2}
 
   data=$(curl \
     --request PUT \
     --silent \
-    ${CONSUL}:8500/v1/kv/database \
-    --data "{
-      \"system_user\": \"${MYSQL_SYSTEM_USER}\",
-      \"root_password\": \"${MYSQL_ROOT_PASS}\"
-    }")
+    ${CONSUL}:8500/v1/kv/${consul_key} \
+    --data ${consul_var})
+#  curl \
+#    --silent \
+#    ${CONSUL}:8500/v1/kv/${consul_key}
+}
 
-  echo "${data}"
+get_consult_var() {
+
+  local consul_key=${1}
 
   data=$(curl \
     --silent \
-    ${CONSUL}:8500/v1/kv/database)
+    ${CONSUL}:8500/v1/kv/${consul_key})
 
-  echo "---------------------------------"
-  echo -e ${data}
-  echo "---------------------------------"
+  if [[ ! -z "${data}" ]]
+  then
+    value=$(echo -e ${data} | jq --raw-output .[].Value 2> /dev/null)
 
-  which base64
-#  data=$(curl \
-#    --silent \
-#    ${CONSUL}:8500/v1/kv/database/root_password)
-#
-#  echo "${data}"
-
+    if [[ ! -z "${value}" ]]
+    then
+      echo ${value} | base64 -d
+    else
+      echo ""
+      #echo "${decoded}"
+    fi
+  fi
 }
 
-register
-
-set +x
+if [[ "${CONFIG_BACKEND}" = "consul" ]]
+then
+  wait_for_consul
+  register_node
+  set_consul_var  'database/root/user' ${MYSQL_SYSTEM_USER}
+  set_consul_var  'database/root/password' ${MYSQL_ROOT_PASS}
+  set_consul_var  'database/url' ${HOSTNAME}
+fi
